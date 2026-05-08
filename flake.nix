b6c6a2b8e29c933a7c237700942c22d28b28ed09
Tim Black <timblaktu@gmail.com>
2026-05-07 20:06:42 -0700

    Add Nix flake with dev shell for building and testing
    Dev shell provides gcc, gnumake, lcov, and Python with all test
    dependencies (cffi, hypothesis, colorama, coloredlogs, docopt,
    terminaltables). CLinkedListQueue is fetched and symlinked
    automatically via shellHook.
    
    Also includes a package output for building the static/shared
    library, and extends .gitignore for build artifacts and nix result.

---
+++ flake.nix
@@ -0,0 +1,75 @@
+{
+  description = "C implementation of the Raft consensus protocol";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        # Python environment for testing (virtraft2, log_fuzzer, cffi bindings)
+        pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+          cffi
+          hypothesis
+          colorama
+          coloredlogs
+          docopt
+          terminaltables
+        ]);
+
+        # CLinkedListQueue - test dependency fetched by `make download-contrib`
+        clinkedlistqueue = pkgs.fetchFromGitHub {
+          owner = "willemt";
+          repo = "CLinkedListQueue";
+          rev = "9f6ca72d0e168178b0faf1b35fb39941fcf7e2c6";
+          hash = "sha256-snvw3nNF1+dEfqhtuU7eMSFS75PveCtfTiiOQzXK3/M=";
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            # Build toolchain
+            gcc
+            gnumake
+
+            # Testing
+            pythonEnv
+
+            # Code coverage
+            lcov
+
+            # Static analysis (optional)
+            # infer
+          ];
+
+          shellHook = ''
+            # Symlink CLinkedListQueue if not already present
+            if [ ! -d CLinkedListQueue ]; then
+              ln -sf ${clinkedlistqueue} CLinkedListQueue
+            fi
+          '';
+        };
+
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "raft";
+          version = "0-unstable";
+          src = ./.;
+
+          buildPhase = ''
+            make static shared
+          '';
+
+          installPhase = ''
+            mkdir -p $out/lib $out/include
+            cp libraft.a $out/lib/
+            cp libraft.so $out/lib/ 2>/dev/null || cp libraft.dylib $out/lib/ 2>/dev/null || true
+            cp include/raft.h include/raft_types.h include/raft_log.h $out/include/
+          '';
+        };
+      }
+    );
+}
